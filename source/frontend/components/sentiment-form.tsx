981fd7327b83e0bdc9796424d689dd5bceb486bc
Steeve-Rodrigue <steeve-rodrigue.takougoum@eleve.ensai.fr>
2026-09-19 09:08:25 +0200

    chore : update sentiment form

--- source/frontend/components/sentiment-form.tsx
+++ source/frontend/components/sentiment-form.tsx
@@ -86,7 +86,7 @@ export function SentimentForm() {
             <span className="text-muted-foreground text-[11px] px-1 sm:text-[14px]">Confidence:
               {result.confidence.toFixed(2)}
             </span>
-            <span className="text-muted-foreground  text-[11px] px-3 sm:text-[14px]"> Time:
+            <span className="text-muted-foreground  text-[11px] px-2 sm:text-[14px]"> Time:
               {(result.processing_time_ms/1000).toFixed(2)} s
             </span>
           </div>
